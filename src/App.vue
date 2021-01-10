84baefee87a4c67a0777454ec6638498e9837ff2
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-11 02:12:41 +0600

    slot is used usng a form L-27

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,39 @@
 <template>
   <div>
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
-    <!-- <hr>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas> -->
-    <app-footer v-bind:title="title"></app-footer> 
+     <form-helper>
+       <div slot="form-header">
+         <h3>This is the title of the form </h3>
+         <p>information about the form</p>
+
+       </div>
+       <div slot="form-fields">
+          <input type="text" placeholder="Name" required>
+          <input type="password" placeholder="Password" required>
+       </div>
+       <div slot="form-control">
+         <button type="submit" v-on:click="handleSubmit"> Submit</button>
+       </div>
+       
+     </form-helper>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header';
-import Ninjas from '@/components/Ninjas';
-import Footer from '@/components/Footer';
+import FormHelper from '@/components/FormHelper';
 export default {
-  components:{
-    'app-header':Header,
-    'app-footer':Footer,
-    'app-ninjas':Ninjas,
+  components: {
+    'form-helper':FormHelper
+    
   },
   data() {
 
     return{
-      ninjas: [
-                {name: 'Ryu', speciality: 'Vue Components', show: false},
-                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-                {name: 'Hitoshi', speciality: 'Click Events', show: false},
-                {name: 'Tango', speciality: 'Conditionals', show: false},
-                {name: 'Kami', speciality: 'Webpack', show: false},
-                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-            ],
-      title:"Vue Ninjas"      
+    
       
     }
    
   },
-  methods:{
-    updateTitle:function(updatedTitle){
-      this.title = updatedTitle
-  } 
-  }  
+    
 }
 </script>
 
---
+++ src/components/FormHelper.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+
+        <h2>Please fill out this form..</h2>
+        <form action="#" method="">
+            <div id="form-header">
+                <slot name="form-header"></slot>
+            </div>
+            <div id="form-fields">
+                <slot name="form-fields"></slot>
+            </div>
+            <div id="form-control">
+                <slot name="form-control"></slot>
+            </div>
+            <div id="useful-links">
+                <ul>
+                    <li><a href="#">Link 1</a></li>
+                    <li><a href="#">Link 2</a></li>
+                    <li><a href="#">Link 3</a></li>
+                </ul>
+            </div>
+        </form>
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    h1,h2{
+    text-align: center;
+}
+form{
+    width: 100%;
+    max-width: 960px;
+    margin: 0 auto;
+}
+#useful-links ul{
+    padding: 0;
+}
+#useful-links li{
+    display: inline-block;
+    margin-right: 10px;
+}
+form > div{
+    padding: 20px;
+    background: #eee;
+    margin: 20px 0;
+}
+#form-header{
+    background: #ddd;
+    border: 1px solid #bbb;
+}
+</style>
