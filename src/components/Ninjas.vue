4dc5fc0b31c05a1641673c5cb2d4ccba6e657f34
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-10 21:56:17 +0600

    Props used,event in child triggers the parents data L-24

--- src/components/Ninjas.vue
+++ src/components/Ninjas.vue
@@ -1,31 +1,39 @@
 
 <template>
     <div id="ninjas">
+    
         <ul>
             <li v-for="ninja in ninjas" v-on:click="ninja.show =!ninja.show" :key="ninja.index">
                 <h2>{{ninja.name}}</h2>
                 <h3 v-show="ninja.show">{{ninja.speciality}}</h3>
             </li>    
         </ul>
+        <button v-on:click="deleteNinja"> Delete Ninja</button>
     </div>
 </template>
 
 <script>
 export default {
 
+    props:{
+        ninjas:{
+            type: Array,
+            required: true,
+          
+        }
+    },
+
     data(){
         return{
-            ninjas: [
-                {name: 'Ryu', speciality: 'Vue Components', show: false},
-                {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-                {name: 'Hitoshi', speciality: 'Click Events', show: false},
-                {name: 'Tango', speciality: 'Conditionals', show: false},
-                {name: 'Kami', speciality: 'Webpack', show: false},
-                {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-            ]
-
+        
+        }
+    },
+    methods:{
+        deleteNinja:function(){
+            this.ninjas.pop();
         }
     }
+  
 
 }
 </script>
