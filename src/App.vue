d9964f215e15498f7ef1b6ffc8df688f6b3dcde7
mdimran1409036 <mdimran1409036@gmail.com>
2021-01-11 16:01:39 +0600

    http request & GET request --JSON placehoder L-33

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
-    <add-blog></add-blog>
-   
+    <!-- <add-blog></add-blog> -->
+    <show-blog></show-blog>   
   </div>
     
 </template>
 
 <script>
-import AddBlog from '@/components/AddBlog'
+// import AddBlog from '@/components/AddBlog'
+import ShowBlog from './components/ShowBlog.vue'
 
 export default {
   components:{
-    'add-blog':AddBlog,
+    // 'add-blog':AddBlog,
+    'show-blog':ShowBlog,
+    
   },
   data() {
 
---
+++ src/components/ShowBlog.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="show-blogs">
+         <h1>All Blog articles</h1>
+         <div class="single-blog" v-for="blog in blogs" :key="blog.index" >
+                 <h2>Title:{{blog.title}}</h2>
+                 <p>Body:</p>
+                 <p>{{blog.body}}</p>
+         </div>
+
+   </div>
+</template>
+
+
+<script>
+
+export default {
+
+
+    data(){
+        return{
+            blogs:[],
+        }
+    },
+    methods:{
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+
+    },
+    created(){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+            this.blogs = data.body.slice(0,10);
+        }) 
+    }
+    
+}
+</script>
+
+<style scoped>
+    #show-blogs{
+        max-width:800px;
+        margin:0 auto;
+    }
+    .single-blog{
+        padding: 20px;
+        margin:  20px 0;
+        box-sizing:border-box;
+        background: #eee ;
+    }
+</style>
